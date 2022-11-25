00bce993fe1bf4bc433f9429585b0f86a703be59
LudwigEffort <dr.feynman65@gmail.com>
2022-11-25 01:47:55 +0100

    add header

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,14 @@ export default {
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  max-width: 1200px;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <img src="@/img/dc-logo.png" alt="">
+    <ul>
+      <li>Characters</li>
+      <li>Comics</li>
+      <li>Movies</li>
+      <li>TV</li>
+      <li>Games</li>
+      <li>Collectibles</li>
+      <li>Videos</li>
+      <li>Fans</li>
+      <li>News</li>
+      <li>Shop</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding: 1rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+header ul {
+  font-size: 1.3rem;
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+}
+header li:hover {
+  cursor: pointer;
+  color: blue;
+}
+</style>
