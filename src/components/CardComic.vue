3010255f1341688156c89021403e45af5367335d
LudwigEffort <dr.feynman65@gmail.com>
2022-11-27 22:05:43 +0100

    add comic images by array

--- src/components/CardComic.vue
+++ src/components/CardComic.vue
@@ -2,25 +2,31 @@
   <div>
     <img
       class="card"
-      src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg"
+      :src="imgUrl"
       alt=""
     >
-    <h2>Tiltle</h2>
+    <h4>{{ series }}</h4>
   </div>
 </template>
 
 <script>
 export default {
   name: 'CardPasta',
+  props: {
+    imgUrl: String,
+    series: String,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
   .card {
     text-align: center;
-    background-color: white;
     img {
       max-width: 90%;
+    }
+    h4 {
+      color: white;
     }
   }
 </style>
